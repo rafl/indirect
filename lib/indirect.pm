554466eac93eb0abec78e190fe175b03eec782f2
Vincent Pit <vince@profvince.com>
2009-06-04 23:54:31 +0200

    This is 0.14

--- lib/indirect.pm
+++ lib/indirect.pm
@@ -11,13 +11,13 @@ indirect - Lexically warn about using the indirect object syntax.
 
 =head1 VERSION
 
-Version 0.13
+Version 0.14
 
 =cut
 
 our $VERSION;
 BEGIN {
- $VERSION = '0.13';
+ $VERSION = '0.14';
 }
 
 =head1 SYNOPSIS
